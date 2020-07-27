5a7997e68ef3d5a6ac9fbb373969a46cc9a37883
Peter Caisse <pcaisse@gmail.com>
2020-07-27 10:43:13 -0400

    Use as-bind to pass high-level data structures to WASM code

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,5 +1,19 @@
 // The entry file of your WebAssembly module.
 
-export function add(a: i32, b: i32): i32 {
-  return a + b;
+export function sum(arr: Int16Array): i32 {
+  let sum = 0;
+  for (let i = 0, k = arr.length; i < k; ++i) {
+    sum += arr[i];
+  }
+  return sum;
+}
+
+export function sumUpTo(x: i32): i32 {
+  let i = 0;
+  let sum = 0;
+  while (i <= x) {
+    sum += i;
+    ++i;
+  }
+  return sum;
 }
