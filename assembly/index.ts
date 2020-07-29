3818a1ca4176636deb99afdb13671c14b9bbe285
Peter Caisse <pcaisse@gmail.com>
2020-07-29 16:28:35 -0400

    Benchmark summing up to a value

--- assembly/index.ts
+++ assembly/index.ts
@@ -7,3 +7,11 @@ export function sum(arr: Int16Array): i32 {
   }
   return sum;
 }
+
+export function sumUpTo(k: i32): i32 {
+  let sum = 0;
+  for (let i = 0; i < k; ++i) {
+    sum += i;
+  }
+  return sum;
+}
